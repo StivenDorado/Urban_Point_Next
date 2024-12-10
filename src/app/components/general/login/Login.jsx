7cdc3be386a0b9b3459e81d81784bb5004839ba9
Stiven Dorado <golds9013@gmail.com>
2024-12-10 14:41:27 -0500

    primer commit

--- src/app/components/general/login/Login.jsx
+++ src/app/components/general/login/Login.jsx
@@ -1,58 +1,182 @@
 import React from 'react';
+import styled from 'styled-components';
 
-const Iniciosesion = () => {
+const Form = () => {
   return (
-    <div className="flex flex-col items-center h-screen">
-      <div className="space-y-4">
-        <h2 className="text-3xl text-center font-bold text-black">Inicia Sesion</h2>
-        {/* Caja de imagenes */}
-        <div className="flex gap-4  items-center justify-center">
-          <img src="../../../public/BxBxlGmail.svg" width="50" alt="Image 2" className="mx-auto" />
-          <img src="../../../public/MdiFacebook.svg" width="45" alt="Image 1" className="mx-auto" />
-          <img src="../../../public/LogosApple.svg" width="33" alt="Image 3" className="mx-auto" />
-        </div>
-        <p className='text-black'>Ingrese su Región y Número de teléfono </p>
-        {/* Formulario de información */}
-        <form class="min-w-full space-y-5">
-          {/* Selecciona país */}
-          <div class="relative w-full">
-            <label for="country" class="sr-only">Country</label>
-            <select id="country" class="w-full pl-10 h-10 rounded-md border border-gray-300 focus:ring focus:ring-blue-500">
-              <option value="(+57)" disabled selected>Colombia (+57)</option>
-              <option value="(+507)">Panama (+507)</option>
-              <option value="(+52)">Mexico (+52)</option>
-              <option value="(+34)">España (+34)</option>
-              <option value="(+593)">Ecuador (+593)</option>
-            </select>
-            <span class="absolute left-3 top-1/2 transform -translate-y-1/2 w-5 h-5 text-gray-500">
-              <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                <path d="M12 2a10 10 0 100 20 10 10 0 000-20z"></path>
-              </svg>
-            </span>
-          </div>
-          {/* Número de teléfono */}
-          <div class="relative w-full">
-            <label for="phone" class="sr-only">Número de teléfono</label>
-            <input id="phone" type="tel" placeholder="Número de teléfono" class="w-full pl-10 h-10 rounded-md border border-gray-300 focus:ring focus:ring-blue-500" />
-            <span class="absolute left-3 top-1/2 transform -translate-y-1/2 w-5 h-5 text-gray-500">
-              <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                <path d="M6.62 10.79a15.91 15.91 0 006.58 6.58l1.83-1.83a1 1 0 011.08-.21 11.72 11.72 0 004.72.91 1 1 0 011 1v3.59a1 1 0 01-1 1A17.93 17.93 0 013 4a1 1 0 011-1h3.6a1 1 0 011 1 11.72 11.72 0 00.91 4.72 1 1 0 01-.21 1.08l-1.83 1.83z"></path>
-              </svg>
-            </span>
-          </div>
+    <StyledWrapper>
+      <div className="form-container">
+        <p className="title">Welcome back</p>
+        <form className="form">
+          <input type="email" className="input" placeholder="Email" />
+          <input type="password" className="input" placeholder="Password" />
+          <p className="page-link">
+            <span className="page-link-label">Forgot Password?</span>
+          </p>
+          <button className="form-btn">Log in</button>
         </form>
-        {/* Botones reguistro */}
-        <br/>
-        <h2 className='text-2xl text-center text-black font-bold'>¡Bienvenido!</h2>
-        
-
-        {/* Botón para iniciar sesión */}
-        <div className="text-center">
-          <h4 className="text-2xl font-bold text-black">Hola!</h4>
-          <button className="mt-4 px-4 py-2 bg-gray-600 text-white rounded-md ">Inicia Sesión</button>
+        <p className="sign-up-label">
+          Don't have an account?<span className="sign-up-link">Sign up</span>
+        </p>
+        <div className="buttons-container">
+          <div className="apple-login-button">
+            <svg stroke="currentColor" fill="currentColor" strokeWidth={0} className="apple-icon" viewBox="0 0 1024 1024" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
+              <path d="M747.4 535.7c-.4-68.2 30.5-119.6 92.9-157.5-34.9-50-87.7-77.5-157.3-82.8-65.9-5.2-138 38.4-164.4 38.4-27.9 0-91.7-36.6-141.9-36.6C273.1 298.8 163 379.8 163 544.6c0 48.7 8.9 99 26.7 150.8 23.8 68.2 109.6 235.3 199.1 232.6 46.8-1.1 79.9-33.2 140.8-33.2 59.1 0 89.7 33.2 141.9 33.2 90.3-1.3 167.9-153.2 190.5-221.6-121.1-57.1-114.6-167.2-114.6-170.7zm-105.1-305c50.7-60.2 46.1-115 44.6-134.7-44.8 2.6-96.6 30.5-126.1 64.8-32.5 36.8-51.6 82.3-47.5 133.6 48.4 3.7 92.6-21.2 129-63.7z" />
+            </svg>
+            <span>Log in with Apple</span>
+          </div>
+          <div className="google-login-button">
+            <svg stroke="currentColor" fill="currentColor" strokeWidth={0} version="1.1" x="0px" y="0px" className="google-icon" viewBox="0 0 48 48" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
+              <path fill="#FFC107" d="M43.611,20.083H42V20H24v8h11.303c-1.649,4.657-6.08,8-11.303,8c-6.627,0-12-5.373-12-12
+      	c0-6.627,5.373-12,12-12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C12.955,4,4,12.955,4,24
+      	c0,11.045,8.955,20,20,20c11.045,0,20-8.955,20-20C44,22.659,43.862,21.35,43.611,20.083z" />
+              <path fill="#FF3D00" d="M6.306,14.691l6.571,4.819C14.655,15.108,18.961,12,24,12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657
+      	C34.046,6.053,29.268,4,24,4C16.318,4,9.656,8.337,6.306,14.691z" />
+              <path fill="#4CAF50" d="M24,44c5.166,0,9.86-1.977,13.409-5.192l-6.19-5.238C29.211,35.091,26.715,36,24,36
+      	c-5.202,0-9.619-3.317-11.283-7.946l-6.522,5.025C9.505,39.556,16.227,44,24,44z" />
+              <path fill="#1976D2" d="M43.611,20.083H42V20H24v8h11.303c-0.792,2.237-2.231,4.166-4.087,5.571
+      	c0.001-0.001,0.002-0.001,0.003-0.002l6.19,5.238C36.971,39.205,44,34,44,24C44,22.659,43.862,21.35,43.611,20.083z" />
+            </svg>
+            <span>Log in with Google</span>
+          </div>
         </div>
       </div>
-    </div>
+    </StyledWrapper>
   );
 }
-export default Iniciosesion;
+
+const StyledWrapper = styled.div`
+  .form-container {
+    width: 350px;
+    height: 500px;
+    background-color: #fff;
+    box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+    border-radius: 10px;
+    box-sizing: border-box;
+    padding: 20px 30px;
+  }
+
+  .title {
+    text-align: center;
+    font-family: "Lucida Sans", "Lucida Sans Regular", "Lucida Grande",
+          "Lucida Sans Unicode", Geneva, Verdana, sans-serif;
+    margin: 10px 0 30px 0;
+    font-size: 28px;
+    font-weight: 800;
+  }
+
+  .form {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    gap: 18px;
+    margin-bottom: 15px;
+  }
+
+  .input {
+    border-radius: 20px;
+    border: 1px solid #c0c0c0;
+    outline: 0 !important;
+    box-sizing: border-box;
+    padding: 12px 15px;
+  }
+
+  .page-link {
+    text-decoration: underline;
+    margin: 0;
+    text-align: end;
+    color: #747474;
+    text-decoration-color: #747474;
+  }
+
+  .page-link-label {
+    cursor: pointer;
+    font-family: "Lucida Sans", "Lucida Sans Regular", "Lucida Grande",
+          "Lucida Sans Unicode", Geneva, Verdana, sans-serif;
+    font-size: 9px;
+    font-weight: 700;
+  }
+
+  .page-link-label:hover {
+    color: #000;
+  }
+
+  .form-btn {
+    padding: 10px 15px;
+    font-family: "Lucida Sans", "Lucida Sans Regular", "Lucida Grande",
+          "Lucida Sans Unicode", Geneva, Verdana, sans-serif;
+    border-radius: 20px;
+    border: 0 !important;
+    outline: 0 !important;
+    background: teal;
+    color: white;
+    cursor: pointer;
+    box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+  }
+
+  .form-btn:active {
+    box-shadow: none;
+  }
+
+  .sign-up-label {
+    margin: 0;
+    font-size: 10px;
+    color: #747474;
+    font-family: "Lucida Sans", "Lucida Sans Regular", "Lucida Grande",
+          "Lucida Sans Unicode", Geneva, Verdana, sans-serif;
+  }
+
+  .sign-up-link {
+    margin-left: 1px;
+    font-size: 11px;
+    text-decoration: underline;
+    text-decoration-color: teal;
+    color: teal;
+    cursor: pointer;
+    font-weight: 800;
+    font-family: "Lucida Sans", "Lucida Sans Regular", "Lucida Grande",
+          "Lucida Sans Unicode", Geneva, Verdana, sans-serif;
+  }
+
+  .buttons-container {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    margin-top: 20px;
+    gap: 15px;
+  }
+
+  .apple-login-button,
+      .google-login-button {
+    border-radius: 20px;
+    box-sizing: border-box;
+    padding: 10px 15px;
+    box-shadow: rgba(0, 0, 0, 0.16) 0px 10px 36px 0px,
+          rgba(0, 0, 0, 0.06) 0px 0px 0px 1px;
+    cursor: pointer;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-family: "Lucida Sans", "Lucida Sans Regular", "Lucida Grande",
+          "Lucida Sans Unicode", Geneva, Verdana, sans-serif;
+    font-size: 11px;
+    gap: 5px;
+  }
+
+  .apple-login-button {
+    background-color: #000;
+    color: #fff;
+    border: 2px solid #000;
+  }
+
+  .google-login-button {
+    border: 2px solid #747474;
+  }
+
+  .apple-icon,
+      .google-icon {
+    font-size: 18px;
+    margin-bottom: 1px;
+  }`;
+
+export default Form;
